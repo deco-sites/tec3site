2707572589028c1752cb9bb634045a87df412eeb
opedrodev <opedro.silva@hotmail.com>
2025-02-02 20:53:43 -0300

    Added cases section

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -6,44 +6,46 @@ import * as $$$$$$$$$$$0 from "./apps/deco/blog.ts";
 import * as $$$$$$$$$$$1 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$2 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/BlogPosts.tsx";
-import * as $$$$$$1 from "./sections/Faq.tsx";
-import * as $$$$$$2 from "./sections/Footer.tsx";
-import * as $$$$$$3 from "./sections/Header.tsx";
-import * as $$$$$$15 from "./sections/hero-1.tsx";
-import * as $$$$$$16 from "./sections/hero-2.tsx";
-import * as $$$$$$4 from "./sections/Hero-servicos.tsx";
-import * as $$$$$$5 from "./sections/Hero-sobre-nos.tsx";
-import * as $$$$$$6 from "./sections/Hero.tsx";
-import * as $$$$$$17 from "./sections/hero2.tsx";
-import * as $$$$$$7 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$8 from "./sections/Logos.tsx";
-import * as $$$$$$9 from "./sections/Portfolio.tsx";
-import * as $$$$$$10 from "./sections/Services.tsx";
-import * as $$$$$$11 from "./sections/Strategy.tsx";
-import * as $$$$$$12 from "./sections/Testimonials.tsx";
-import * as $$$$$$13 from "./sections/Theme/Theme.tsx";
-import * as $$$$$$14 from "./sections/WhyTec3.tsx";
+import * as $$$$$$1 from "./sections/Cases.tsx";
+import * as $$$$$$2 from "./sections/Faq.tsx";
+import * as $$$$$$3 from "./sections/Footer.tsx";
+import * as $$$$$$4 from "./sections/Header.tsx";
+import * as $$$$$$16 from "./sections/hero-1.tsx";
+import * as $$$$$$17 from "./sections/hero-2.tsx";
+import * as $$$$$$5 from "./sections/Hero-servicos.tsx";
+import * as $$$$$$6 from "./sections/Hero-sobre-nos.tsx";
+import * as $$$$$$7 from "./sections/Hero.tsx";
+import * as $$$$$$18 from "./sections/hero2.tsx";
+import * as $$$$$$8 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$9 from "./sections/Logos.tsx";
+import * as $$$$$$10 from "./sections/Portfolio.tsx";
+import * as $$$$$$11 from "./sections/Services.tsx";
+import * as $$$$$$12 from "./sections/Strategy.tsx";
+import * as $$$$$$13 from "./sections/Testimonials.tsx";
+import * as $$$$$$14 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$15 from "./sections/WhyTec3.tsx";
 
 const manifest = {
   "sections": {
     "site/sections/BlogPosts.tsx": $$$$$$0,
-    "site/sections/Faq.tsx": $$$$$$1,
-    "site/sections/Footer.tsx": $$$$$$2,
-    "site/sections/Header.tsx": $$$$$$3,
-    "site/sections/hero-1.tsx": $$$$$$15,
-    "site/sections/hero-2.tsx": $$$$$$16,
-    "site/sections/Hero-servicos.tsx": $$$$$$4,
-    "site/sections/Hero-sobre-nos.tsx": $$$$$$5,
-    "site/sections/Hero.tsx": $$$$$$6,
-    "site/sections/hero2.tsx": $$$$$$17,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$7,
-    "site/sections/Logos.tsx": $$$$$$8,
-    "site/sections/Portfolio.tsx": $$$$$$9,
-    "site/sections/Services.tsx": $$$$$$10,
-    "site/sections/Strategy.tsx": $$$$$$11,
-    "site/sections/Testimonials.tsx": $$$$$$12,
-    "site/sections/Theme/Theme.tsx": $$$$$$13,
-    "site/sections/WhyTec3.tsx": $$$$$$14,
+    "site/sections/Cases.tsx": $$$$$$1,
+    "site/sections/Faq.tsx": $$$$$$2,
+    "site/sections/Footer.tsx": $$$$$$3,
+    "site/sections/Header.tsx": $$$$$$4,
+    "site/sections/hero-1.tsx": $$$$$$16,
+    "site/sections/hero-2.tsx": $$$$$$17,
+    "site/sections/Hero-servicos.tsx": $$$$$$5,
+    "site/sections/Hero-sobre-nos.tsx": $$$$$$6,
+    "site/sections/Hero.tsx": $$$$$$7,
+    "site/sections/hero2.tsx": $$$$$$18,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$8,
+    "site/sections/Logos.tsx": $$$$$$9,
+    "site/sections/Portfolio.tsx": $$$$$$10,
+    "site/sections/Services.tsx": $$$$$$11,
+    "site/sections/Strategy.tsx": $$$$$$12,
+    "site/sections/Testimonials.tsx": $$$$$$13,
+    "site/sections/Theme/Theme.tsx": $$$$$$14,
+    "site/sections/WhyTec3.tsx": $$$$$$15,
   },
   "apps": {
     "site/apps/deco/blog.ts": $$$$$$$$$$$0,
---
+++ sections/Cases.tsx
@@ -0,0 +1,54 @@
+import { ImageWidget } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
+
+interface Case {
+  mockup: ImageWidget;
+  link: string;
+}
+
+interface Props {
+  title: string;
+  description: string;
+  /** @maxItems 4 */
+  cases: Case[]
+  cta?: string;
+}
+
+export default function Section({ title, description, cases, cta = "Ver mais" }: Props) {
+  return (
+    <div className="px-4 py-4 mb-32 bg-[#cbcbcb41] lg:py-8">
+      <h2 class="mb-3 text-sm leading-none font-bold inline-block uppercase text-transparent bg-clip-text bg-gradient-to-r from-[#F7362D] to-[#ED8123] lg:px-28 lg:text-2xl">
+        {title}
+      </h2>
+
+      <p className="text-xs mb-4 max-w-[750px] lg:px-28 lg:text-xl">{description}</p>
+
+      <div className="h-64 w-full bg-red-500 mb-4 lg:h-fit lg:bg-transparent lg:grid lg:grid-cols-4">
+        {/* TODO: carousel */}
+
+        {
+          cases.map((c) => (
+            <a
+              href="/portifolio"
+              className="hidden lg:block"
+            >
+              <Image
+                width={425}
+                height={425}
+                src={c.mockup}
+                className="max-w-[425px] max-h-[425px] w-full h-full"
+              />
+            </a>
+          ))
+        }
+      </div>
+
+      <a
+        href="/portifolio"
+        className="block w-fit py-2 px-14 text-white text-xs mx-auto bg-gradient-to-r from-[#CD6209] to-[#F81504] lg:text-xl"
+      >
+        {cta}
+      </a>
+    </div>
+  )
+}
